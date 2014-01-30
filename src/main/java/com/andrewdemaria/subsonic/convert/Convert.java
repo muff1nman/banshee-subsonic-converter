518572926138504c4fad929bab0de6e09f70fa8a
Andrew DeMaria <ademaria@mines.edu>
2014-01-30 12:50:12 -0700

    maven file structure added

---
+++ src/main/java/com/andrewdemaria/subsonic/convert/Convert.java
@@ -0,0 +1,14 @@
+/*
+ * Convert.java
+ * Copyright (C) 2014 Andrew DeMaria (muff1nman) <ademaria@mines.edu>
+ *
+ * All Rights Reserved.
+ */
+
+public interface Convert {
+
+	public void convert_database( File database_in, File database_out );
+
+}
+
+
