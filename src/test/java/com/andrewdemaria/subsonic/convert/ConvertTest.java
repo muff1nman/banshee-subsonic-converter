5e2b3046d71b9c03145f361df62629754f5832b2
Andrew DeMaria <ademaria@mines.edu>
2014-01-31 17:39:49 -0700

    Use junit 4

---
+++ src/test/java/com/andrewdemaria/subsonic/convert/ConvertTest.java
@@ -0,0 +1,13 @@
+package com.andrewdemaria.subsonic.convert;
+
+import org.junit.Test;
+
+/**
+ * Created by andrew on 1/31/14.
+ */
+public class ConvertTest {
+    @Test
+    public void BansheeSqliteFileCanBeOpened() {
+
+    }
+}
